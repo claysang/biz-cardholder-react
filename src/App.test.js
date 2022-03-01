00f5021baa774402a022d48f42325dc7bcd409d6
Chao Sang <csang@thoughtworks.com>
2022-03-01 15:15:27 +0800

    Fix test again

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Add Name/);
+  const linkElement = screen.getAllByText(/Add Name/);
   expect(linkElement).toBeInTheDocument();
 });
