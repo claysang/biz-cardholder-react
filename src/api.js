8554ce27a18373aa94515d1af83406c13da2fece
Chao Sang <csang@thoughtworks.com>
2022-03-04 15:08:22 +0800

    Add PROD deployment

--- src/api.js
+++ src/api.js
@@ -1,4 +1,8 @@
-const url = 'http://localhost:65432/api/card';
+const testUrl = 'http://localhost:65432/api/card';
+const prodUrl = 'http://localhost:65123/api/card';
+
+const url = process.env.NODE_ENV === 'production' ? prodUrl : testUrl
+console.log("Current env", process.env.NODE_ENV);
 
 export const getCardReq = async () => {
   const res = await fetch(url);
