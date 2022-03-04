a7dfcbb93ef933583ab6181e3403d0db41ad8c73
Clay Sang <cs@claysang.com>
2022-03-04 18:43:38 +0800

    Buildkite Integration (#1)
    * Add Buildkite config
    
    * Fix indentation issue
    
    * Add different Dockerfile and modify Buildkite
    
    * Modify Buildkite config
    
    * Modify Buildkite parallelism
    
    * Buildkite test
    
    * Add build context for docker-compose.pipeline
    
    * Revert "Buildkite test"
    
    This reverts commit 247b23d1f9cacccacb0184f37df700e630ef699f.
    
    * Fix test
    
    * Fix test again
    
    * No watch for running UT on pipeline
    
    * Fix test again
    
    * Add pipelineTest command
    
    * Add image-repository to allow using pre-build image
    
    * Comment out image-repository
    
    * Add artifact
    
    * Mount build volume
    
    * Add Deploy steps
    
    * Fix error
    
    * Modify deploy step command
    
    * Add ops folder
    
    * Modify deploy script
    
    * Modify buildkite scripts
    
    * Modify scripts again
    
    * Format code
    
    * Add PROD deployment
    
    * Refine build env
    
    * Refine env build script
    
    * Refine scripts
    
    Co-authored-by: Chao Sang <csang@thoughtworks.com>

--- src/api.js
+++ src/api.js
@@ -1,4 +1,8 @@
-const url = 'http://localhost:65432/api/card';
+const testUrl = 'http://localhost:65432/api/card';
+const prodUrl = 'http://localhost:65123/api/card';
+
+const url = process.env.REACT_APP_DP_ENV === 'production' ? prodUrl : testUrl
+console.log("Current env", process.env.REACT_APP_DP_ENV);
 
 export const getCardReq = async () => {
   const res = await fetch(url);
